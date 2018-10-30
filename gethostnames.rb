dcfb9bff4e1985d3a0d089b4838af7954e70bfdc
Philip Winnington <ftcpjw@yahoo.com>
2018-10-29 21:51:48 -0400

    fixed an issue with the gethostnames script

--- gethostnames.rb
+++ gethostnames.rb
@@ -1,10 +1,11 @@
+#require resolv for nslookups
 require 'resolv'
 
-ips = ['8.8.8.8','4.4.4.4']
+#list of ip addresses
+ips = ['8.8.8.8','192.168.1.1']
 
+#get hostname from each ip address in the list
 for x in ips
-  arr = []
   puts Resolv.new.getname x
 end
-# puts ips.map(|x| Resolv.new.getname 'x')
 
