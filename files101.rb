0c9cfa4bc204af8a40213b76b78b8f772e1e4b34
Philip Winnington <ftcpjw@yahoo.com>
2018-12-01 19:01:06 -0500

    added a couple file based scripts

---
+++ files101.rb
@@ -0,0 +1,15 @@
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "Here's your file #{filename}"
+puts txt.read
+txt.close
+
+print "Type the file name again: "
+file_again =$stdin.gets.chomp 
+
+txt_again = open(file_again)
+
+print txt_again.read
+txt_again.close
